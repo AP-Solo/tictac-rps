3f2f0120a33c5b9600315ac3f3a564fd2af34b6b
AP-Solo <Anas_Parker@yahoo.com>
2021-07-04 14:30:35 +0000

    add function plan

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function startgame(){
+
+}
+
+function cells(){
+//rock papper sissors option inside each cell
+}
+
+function gameStatus(){
+// win, lose or draw
+}
+
+function restartMatch(){
+// rematch (reset the game)
+}
+
+function quitgame(){
+        
+}
+function winningCombinations(){
+    
+}
