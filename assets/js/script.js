2735e3c0fc08b1ec86db97d86eabe536dc951273
AP-Solo <Anas_Parker@yahoo.com>
2021-07-10 21:57:05 +0000

    add game start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-const circle = "O";
-const cross = "x";
-const boxes = document.getElementsByClassName('class');
-for(i=0; i < boxes.length; i++){
-    boxes.addEventListner('click', buttonClick);
-    
+const board = document.querySelector('#game-board');
+const cubes = document.querySelectorAll('#game-board .cell');
+
+//Start of game
+gameStart();
+
+function gameStart(){
+    board.classList.remove('turn-x', 'turn-o');
+
+    for(let cell of cubes){
+        cell.classList.remove('x', 'o');
+        cell.addEventListener('click', selection {once: true});
+    }
+    currentPlayer = math.round(0,1) === 1 ? 'x' , 'o';
+    board.classList.add('turn-' + currentPlayer);
+};
+
+function selection(){
+    this.classList.add(currentPlayer);
 }
 
-
-// Game status
 const winningPatterns = [
     [0,1,2],
     [3,4,5],
@@ -17,20 +28,27 @@ const winningPatterns = [
     [2,5,8],
     [0,4,8],
     [2,4,6]
-]
+];
 
-function result(){
-    if (winningPatterns){
-        console.log("Winner");}
-        else{
-            console.log("Tie Game")
-        };
-}
-function buttonClick(event){
-    console.log(circle);
+function buttonClick(){
+    this.classList.add(playerSwitch);
+
+    playerSwitch = playerSwitch == 'x' ? '0': 'x';
+    board.classList.remove('turn-o', 'turn-x');
+    board.classList.add('turn-o', + playerSwitch);
 }
 
+function decideResult(){
+    return winningPatterns.some(winningPatterns => {
+           return winningPatterns.every(w => {
+               if(cubes[w].classList.contains(currentPlayer)) {
+                   return true;
+               }
 
+               return false;
+           })
+    });
+}
 
 //game start
 //player selection x,o
@@ -38,9 +56,3 @@ function buttonClick(event){
 //switch player
 //decide result
 //Rematch option
-
-function gameStart(){}
-function playerSelect(){}
-function playerSwitch(){}
-function decideResult(){}
-function rematch(){}
